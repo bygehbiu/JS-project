60c7503106ad7f3d6602df46e13c87f8cbec1874
Kostya <bygehbiu@gmail.com>
2022-12-05 13:00:23 +0300

    first commit

---
+++ js/01_Переменные и строгий режим.js
@@ -0,0 +1,30 @@
+/* **** Переменные и строгий режим **** */
+
+"use strict"; // работаем в современном режиме
+
+a = 14;
+console.log(a);
+
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+const obj = {
+    a: 50
+
+};
+obj.a = 10;
+console.log(obj);
+
+
+var name = 'Ivan';
+console.log(name);
+
+
+{
+    var result = 50;
+}
+console.log(result);
---
+++ js/04_Интерполяция.js
@@ -0,0 +1,9 @@
+/* **** Интерполяция **** */
+
+"use strict"; // работаем в современном режиме
+
+const category = 'toys';
+console.log(`https://someurl.com/${category}/5`); // нужно использовать косые кавычки
+
+const user = "Ivan";
+alert(`Привет, ${user}`);
